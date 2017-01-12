97d71eefc2bde845a659b3d5c970024b0485d000
Ben Kreeger <ben.kreeger@icloud.com>
2017-01-12 15:31:41 -0600

    Make podfile point at our version, remove duplicate code.

--- SWRevealViewController/SWRevealViewController.m
+++ SWRevealViewController/SWRevealViewController.m
@@ -489,12 +489,6 @@ static CGFloat scaledValue( CGFloat v1, CGFloat min2, CGFloat max2, CGFloat min1
     return _view.bounds;
 }
 
-- (CGAffineTransform)targetTransform
-{
-    return self.targetTransform;
-}
-
-
 
 @end
 
